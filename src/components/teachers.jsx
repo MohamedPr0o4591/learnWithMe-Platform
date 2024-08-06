d38cd882ebdd049be5344333572cbfd1eb6176f4
mohamedpr0o4591 <ev9ry.24hours@gmail.com>
2024-08-07 00:59:36 +0300

    update

--- src/components/teachers.jsx
+++ src/components/teachers.jsx
@@ -2,12 +2,18 @@ import mohamedNasr from "../assets/img1.png";
 import mohamedNasrCvr from "../assets/img2.jpg";
 import tasniem from "../assets/img3.png";
 import tasniemCvr from "../assets/img4.jpg";
-import shrouq from "../assets/img6.png";
+import shrouq from "../assets/img7.png";
 import shrouqCvr from "../assets/img5.jpg";
 import basmala from "../assets/img9.png";
 import basmalaCvr from "../assets/img8.jpg";
 import Samira from "../assets/img10.png";
 import SamiraCvr from "../assets/img11.jpg";
+import mohamedMokhtar from "../assets/img13.png";
+import mohamedMokhtarCvr from "../assets/img12.jpg";
+import basmalaMahmoud from "../assets/img15.png";
+import basmalaMahmoudCvr from "../assets/img14.jpg";
+import ahmedSamy from "../assets/img17.png";
+import ahmedSamyCvr from "../assets/img16.jpg";
 
 export const teachersData = [
   {
@@ -22,6 +28,19 @@ export const teachersData = [
     fbLink: "https://wa.me/+201009300000",
     tgLink: "https://wa.me/+201009300000",
   },
+
+  // {
+  //   name: "Mohamed Mokhtar",
+  //   title: "FrontEnd Developer",
+  //   img: mohamedMokhtar,
+  //   imgCvr: mohamedMokhtarCvr,
+  //   imgLeft: "-13px",
+  //   imgWidth: "90%",
+  //   cvrPos: "center center",
+  //   waLink: "https://wa.me/+201009300000",
+  //   fbLink: "https://wa.me/+201009300000",
+  //   tgLink: "https://wa.me/+201009300000",
+  // },
   {
     name: "Tasniem Mosaad",
     title: "Teacher of English",
@@ -66,6 +85,30 @@ export const teachersData = [
     imgLeft: "-13px",
     imgWidth: "100%",
     cvrPos: "center center",
+    waLink: "https://wa.me/+201009300000",
+    fbLink: "https://wa.me/+201009300000",
+    tgLink: "https://wa.me/+201009300000",
+  },
+  {
+    name: "Basmala Mahmoud",
+    title: "Teacher of Maths",
+    img: basmalaMahmoud,
+    imgCvr: basmalaMahmoudCvr,
+    imgLeft: "13px",
+    imgWidth: "90%",
+    cvrPos: "center center",
+    waLink: "https://wa.me/+201009300000",
+    fbLink: "https://wa.me/+201009300000",
+    tgLink: "https://wa.me/+201009300000",
+  },
+  {
+    name: "Ahmed Samy",
+    title: "Teacher of Maths",
+    img: ahmedSamy,
+    imgCvr: ahmedSamyCvr,
+    imgLeft: "-10px",
+    imgWidth: "100%",
+    cvrPos: "top",
     waLink: "https://wa.me/+201009300000",
     fbLink: "https://wa.me/+201009300000",
     tgLink: "https://wa.me/+201009300000",
