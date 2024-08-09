8a7b4c0d16e70242eea7aac2c8f97de59d6d2ae9
mohamedpr0o4591 <ev9ry.24hours@gmail.com>
2024-08-09 04:37:06 +0300

    update

--- src/components/pages/home/contact/ContactComponent.jsx
+++ src/components/pages/home/contact/ContactComponent.jsx
@@ -1,32 +1,67 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./ContactComponent.css";
+import emailjs from "@emailjs/browser";
 
 function ContactComponent() {
+  const form = useRef();
+  const [loading, setLoading] = useState(false);
+
+  function sendEmail(e) {
+    let flag;
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .sendForm("service_vb5d7ut", "template_kdt464i", form.current, {
+        publicKey: "D4Z9I3jewjCxeVQ1T",
+      })
+      .then(
+        (_) => {
+          alert("تم الارسال بنجاح");
+        },
+        (error) => {
+          alert("حدث خطأ اثناء الارسال" + error.text);
+        }
+      );
+
+    setLoading(false);
+  }
+
   return (
     <div className="contact-component" id="contact">
-      <form>
+      <form ref={form} onSubmit={sendEmail}>
         <h3>تواصل معنا</h3>
 
         <div className="input-box">
           <label htmlFor="name">الاسم</label>
-          <input type="text" id="name" />
+          <input type="text" id="name" name="from_name" />
         </div>
 
         <div className="input-box">
           <label htmlFor="email" className="required">
             البريد الالكتروني
           </label>
-          <input type="text" required id="email" className="input-ltr" />
+          <input
+            name="from_email"
+            type="text"
+            required
+            id="email"
+            className="input-ltr"
+          />
         </div>
 
         <div className="input-box">
           <label htmlFor="content" className="required">
             الرسالة
           </label>
-          <textarea id="content" required />
+          <textarea id="content" name="message" required />
         </div>
 
-        <input type="submit" value="ارسال" />
+        <input
+          type="submit"
+          value={`${loading ? "جاري الإرسال .." : "ارسال"}`}
+          className={`${loading ? "btn-disabled" : ""}`}
+        />
       </form>
     </div>
   );
